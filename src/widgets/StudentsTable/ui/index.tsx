96994d0aae18a481f486e8192629e72bb1e863ca
cvazy <player7712@mail.ru>
2024-09-25 01:20:31 +0300

    Добавил таблицу сотрудников

---
+++ src/widgets/StudentsTable/ui/index.tsx
@@ -0,0 +1,361 @@
+import { AgGridReact } from "@ag-grid-community/react";
+import React, { useEffect, useMemo, useState } from "react";
+import type { ColDef } from "@ag-grid-community/core";
+import { useTranslation } from "react-i18next";
+
+export const StudentsTable = () => {
+  const { t, i18n } = useTranslation("tablesPage");
+
+  const [rowData] = useState([
+    {
+      make: "Tesla",
+      model: "Model Y",
+      price: 64950,
+      electric: true,
+      month: "June",
+    },
+    {
+      make: "Ford",
+      model: "F-Series",
+      price: 33850,
+      electric: false,
+      month: "October",
+    },
+    {
+      make: "Toyota",
+      model: "Corolla",
+      price: 29600,
+      electric: false,
+      month: "August",
+    },
+    {
+      make: "Mercedes",
+      model: "EQA",
+      price: 48890,
+      electric: true,
+      month: "February",
+    },
+    {
+      make: "Fiat",
+      model: "500",
+      price: 15774,
+      electric: false,
+      month: "January",
+    },
+    {
+      make: "Nissan",
+      model: "Juke",
+      price: 20675,
+      electric: false,
+      month: "March",
+    },
+    {
+      make: "Vauxhall",
+      model: "Corsa",
+      price: 18460,
+      electric: false,
+      month: "July",
+    },
+    {
+      make: "Volvo",
+      model: "EX30",
+      price: 33795,
+      electric: true,
+      month: "September",
+    },
+    {
+      make: "Mercedes",
+      model: "Maybach",
+      price: 175720,
+      electric: false,
+      month: "December",
+    },
+    {
+      make: "Vauxhall",
+      model: "Astra",
+      price: 25795,
+      electric: false,
+      month: "April",
+    },
+    {
+      make: "Fiat",
+      model: "Panda",
+      price: 13724,
+      electric: false,
+      month: "November",
+    },
+    {
+      make: "Jaguar",
+      model: "I-PACE",
+      price: 69425,
+      electric: true,
+      month: "May",
+    },
+    {
+      make: "Tesla",
+      model: "Model Y",
+      price: 64950,
+      electric: true,
+      month: "June",
+    },
+    {
+      make: "Ford",
+      model: "F-Series",
+      price: 33850,
+      electric: false,
+      month: "October",
+    },
+    {
+      make: "Toyota",
+      model: "Corolla",
+      price: 29600,
+      electric: false,
+      month: "August",
+    },
+    {
+      make: "Mercedes",
+      model: "EQA",
+      price: 48890,
+      electric: true,
+      month: "February",
+    },
+    {
+      make: "Fiat",
+      model: "500",
+      price: 15774,
+      electric: false,
+      month: "January",
+    },
+    {
+      make: "Nissan",
+      model: "Juke",
+      price: 20675,
+      electric: false,
+      month: "March",
+    },
+    {
+      make: "Vauxhall",
+      model: "Corsa",
+      price: 18460,
+      electric: false,
+      month: "July",
+    },
+    {
+      make: "Volvo",
+      model: "EX30",
+      price: 33795,
+      electric: true,
+      month: "September",
+    },
+    {
+      make: "Mercedes",
+      model: "Maybach",
+      price: 175720,
+      electric: false,
+      month: "December",
+    },
+    {
+      make: "Vauxhall",
+      model: "Astra",
+      price: 25795,
+      electric: false,
+      month: "April",
+    },
+    {
+      make: "Fiat",
+      model: "Panda",
+      price: 13724,
+      electric: false,
+      month: "November",
+    },
+    {
+      make: "Jaguar",
+      model: "I-PACE",
+      price: 69425,
+      electric: true,
+      month: "May",
+    },
+    {
+      make: "Tesla",
+      model: "Model Y",
+      price: 64950,
+      electric: true,
+      month: "June",
+    },
+    {
+      make: "Ford",
+      model: "F-Series",
+      price: 33850,
+      electric: false,
+      month: "October",
+    },
+    {
+      make: "Toyota",
+      model: "Corolla",
+      price: 29600,
+      electric: false,
+      month: "August",
+    },
+    {
+      make: "Mercedes",
+      model: "EQA",
+      price: 48890,
+      electric: true,
+      month: "February",
+    },
+    {
+      make: "Fiat",
+      model: "500",
+      price: 15774,
+      electric: false,
+      month: "January",
+    },
+    {
+      make: "Nissan",
+      model: "Juke",
+      price: 20675,
+      electric: false,
+      month: "March",
+    },
+    {
+      make: "Vauxhall",
+      model: "Corsa",
+      price: 18460,
+      electric: false,
+      month: "July",
+    },
+    {
+      make: "Volvo",
+      model: "EX30",
+      price: 33795,
+      electric: true,
+      month: "September",
+    },
+    {
+      make: "Mercedes",
+      model: "Maybach",
+      price: 175720,
+      electric: false,
+      month: "December",
+    },
+    {
+      make: "Vauxhall",
+      model: "Astra",
+      price: 25795,
+      electric: false,
+      month: "April",
+    },
+    {
+      make: "Fiat",
+      model: "Panda",
+      price: 13724,
+      electric: false,
+      month: "November",
+    },
+    {
+      make: "Jaguar",
+      model: "I-PACE",
+      price: 69425,
+      electric: true,
+      month: "May",
+    },
+  ]);
+
+  const [columnDefs, setColumnDefs] = useState<ColDef[]>([]);
+
+  const localeText = useMemo(
+    () => ({
+      equals: t("agGrid.filter.equals"),
+      notEqual: t("agGrid.filter.notEqual"),
+      lessThan: t("agGrid.filter.lessThan"),
+      greaterThan: t("agGrid.filter.greaterThan"),
+      contains: t("agGrid.filter.contains"),
+      notContains: t("agGrid.filter.doesNotContain"),
+      startsWith: t("agGrid.filter.startsWith"),
+      endsWith: t("agGrid.filter.endsWith"),
+      applyFilter: t("agGrid.filter.applyFilter"),
+      resetFilter: t("agGrid.filter.resetFilter"),
+      clearFilter: t("agGrid.filter.clearFilter"),
+      notBlank: t("agGrid.filter.notBlank"),
+      blank: t("agGrid.filter.blank"),
+    }),
+    [t],
+  );
+
+  useEffect(() => {
+    setColumnDefs([
+      {
+        field: "make",
+        editable: true,
+        headerName: t("agGrid.columns.make"),
+        cellEditor: "agSelectCellEditor",
+        cellEditorParams: {
+          values: [
+            "Tesla",
+            "Ford",
+            "Toyota",
+            "Mercedes",
+            "Fiat",
+            "Nissan",
+            "Vauxhall",
+            "Volvo",
+            "Jaguar",
+          ],
+        },
+        flex: 1,
+      },
+      { field: "model", headerName: t("agGrid.columns.model"), flex: 1 },
+      {
+        field: "price",
+        headerName: t("agGrid.columns.price"),
+        filter: "agNumberColumnFilter",
+        flex: 1,
+      },
+      { field: "electric", headerName: t("agGrid.columns.electric"), flex: 1 },
+      {
+        field: "month",
+        headerName: t("agGrid.columns.month"),
+        comparator: (valueA: string, valueB: string) => {
+          const months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ];
+          return months.indexOf(valueA) - months.indexOf(valueB);
+        },
+        flex: 1,
+      },
+    ]);
+  }, [t, i18n.language]);
+
+  const defaultColDef = useMemo(
+    () => ({
+      filter: "agTextColumnFilter",
+      floatingFilter: true,
+    }),
+    [],
+  );
+
+  return (
+    <div className={"ag-theme-quartz w-full h-[512px]"}>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={columnDefs}
+        defaultColDef={defaultColDef}
+        localeText={localeText}
+        pagination={true}
+        paginationPageSize={10}
+        paginationPageSizeSelector={[10, 25, 50]}
+      />
+    </div>
+  );
+};
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -2,3 +2,5 @@ export * from "./Description";
 export * from "./Distribution";
 export * from "./Header";
 export * from "./GraduateRates";
+export * from "./EmployeesTable";
+export * from "./StudentsTable";
